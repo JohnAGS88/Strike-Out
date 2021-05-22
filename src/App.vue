cd5c01becbbe5637e5af8e3c8e9cb806fb1bbfaa
John Goyo <jgoyosilva@gmail.com>
2021-05-21 22:36:33 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <HelloWorld msg="Bienvenidos a Strike-Out App"/>
+  <form class="shadow-lg p-3 mb-5 bg-white rounded">
+  <div class="form-row">
+    <div class="col-md-5 mb-3"></div>
+    <div class="col-md-2 mb-3">
+      <label for="validationServer"></label>
+      <input type="text" class="form-control is-valid" id="validationServer02" placeholder="Nombre y Apellido"  required>
+      <div class="valid-feedback">
+        Ingrese Sus Datos
+      </div>
+    </div>
+    <div class="col-md-4 mb-3"></div>
+  </div>
+  <button class="btn btn-dark" type="submit">Aceptar</button>
+</form>
+  <datepicker></datepicker>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Datepicker from 'vuejs-datepicker';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Datepicker
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Nunito', sans-serif; 
+  text-align: center;
+  color: #072b50;
+}
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <nav class="navbar  bg-dark">
+      <a class="navbar-brand" href="#"></a>
+    </nav>
+    <h1>{{ msg }}</h1> 
+  </div>
+  <main class="container">
+    <div class="row" style="padding-top: 100px">
+        <div class="col">
+            <input data-date-format="dd/mm/yyyy" id="datepicker">
+        </div>
+    </div>
+</main>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+
+</script>
+
+<!-- Para Estilos -->
+<style scoped>
+
+</style>
